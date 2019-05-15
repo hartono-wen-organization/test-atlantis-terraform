dc59604a17e95cda6dfcd2f0d69c3d1565fc87c7
hartono-wen <hartono.wen@gmail.com>
2019-05-15 14:35:33 +0700

    test

---
+++ terraform/aws/s3/hartono-test-atlantis-temp.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "b" {
+    bucket = "hartono-test-atlantis-temp"
+    acl    = "private"
+
+    tags = {
+        Name        = "hartono-test-atlantis-temp"
+        Environment = "Test"
+        JIRA        = "IK-369 Secure Atlantis server"
+    }
+
+    versioning {
+        enabled = false
+    }
+}
